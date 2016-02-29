fc6c58aeba11cd2a91ab8410cc882cd682a5189b
Kenneth Glawe <kenneth.glawe@gmail.com>
2016-02-29 12:44:16 +0100

    Created and began work on the board.

---
+++ Game of Life/Board.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_of_Life
+{
+    class Board
+    {
+        public const int Size = 100;
+        private int[,] _board;
+
+        public Board()
+        {
+            _board = new int[Size,Size];
+        }
+    }
+}
